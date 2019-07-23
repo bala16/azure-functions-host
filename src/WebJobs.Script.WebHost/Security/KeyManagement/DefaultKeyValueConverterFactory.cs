79250e7290f1e28a879d0f7e1ccbd426c577f973
Bala <balag@outlook.com>
2019-07-22 17:28:16 -0700

    Enable

--- src/WebJobs.Script.WebHost/Security/KeyManagement/DefaultKeyValueConverterFactory.cs
+++ src/WebJobs.Script.WebHost/Security/KeyManagement/DefaultKeyValueConverterFactory.cs
@@ -23,12 +23,6 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
 
         private static bool IsEncryptionSupported()
         {
-            if (SystemEnvironment.Instance.IsLinuxContainerEnvironment())
-            {
-                // TEMP: https://github.com/Azure/azure-functions-host/issues/3035
-                return false;
-            }
-
             return SystemEnvironment.Instance.IsAppServiceEnvironment() ||
                 SystemEnvironment.Instance.GetEnvironmentVariable(AzureWebsiteLocalEncryptionKey) != null;
         }
