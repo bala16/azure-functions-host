c5afeef0998cbf81218da30696fd3b4d0b952376
Bala <balag@outlook.com>
2019-08-29 13:56:15 -0700

    Add WorkerValidationMiddleware

---
+++ src/WebJobs.Script.WebHost/Middleware/WorkerValidationMiddleware.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Azure.WebJobs.Script.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Middleware
+{
+    public class WorkerValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IEnvironment _environment;
+        private readonly ILogger<WorkerValidationMiddleware> _logger;
+
+        public WorkerValidationMiddleware(RequestDelegate next, IEnvironment environment, ILogger<WorkerValidationMiddleware> logger)
+        {
+            _next = next;
+            _environment = environment;
+            _logger = logger;
+        }
+
+        private bool IsWrongWorker(HttpRequest request)
+        {
+            var runtimeSiteName = _environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName);
+            var siteDeploymentId = request.Headers[ScriptConstants.AntaresSiteDeploymentId];
+
+            // Incoming request is for a specific site.
+            if (!string.IsNullOrEmpty(siteDeploymentId))
+            {
+                // There are 2 scenarios the request should be blocked
+                // 1. current container hasn't been specialized yet.
+                // (there could be a race between specialization and cold start request). So this middleware needs to be behind EnvironmentReadyCheckMiddleware
+                // 2. current container is assigned to a different site.
+
+                if (string.IsNullOrEmpty(runtimeSiteName) ||
+                    !string.Equals(runtimeSiteName, siteDeploymentId, StringComparison.OrdinalIgnoreCase))
+                {
+                    _logger.LogWarning("Request for site {0} received by container specialized for site {1}",
+                        siteDeploymentId, runtimeSiteName);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (IsWrongWorker(context.Request))
+            {
+                _logger.LogInformation("Short circuiting request for " + context.Request.Path);
+                using (var writer = new StreamWriter(context.Response.Body))
+                {
+                    context.Response.Headers.Add("X-INVALIDATE-CACHE", "1");
+                    context.Response.StatusCode = 503;
+                    await writer.WriteAsync(string.Empty);
+                }
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ test/WebJobs.Script.Tests/Middleware/WorkerValidationMiddlewareTests.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Azure.WebJobs.Script.WebHost.Middleware;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Microsoft.WebJobs.Script.Tests;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
+{
+    public class WorkerValidationMiddlewareTests
+    {
+        private readonly TestLoggerProvider _loggerProvider;
+        private readonly LoggerFactory _loggerFactory = new LoggerFactory();
+
+        public WorkerValidationMiddlewareTests()
+        {
+            _loggerProvider = new TestLoggerProvider();
+            _loggerFactory.AddProvider(_loggerProvider);
+        }
+
+        [Fact]
+        public async Task Blocks_Request_For_Wrong_Worker()
+        {
+            RequestDelegate requestDelegate = async (HttpContext context) =>
+            {
+                await Task.Delay(0);
+                throw new Exception("Middleware exception");
+            };
+
+            var mockEnvironment = new Mock<IEnvironment>(MockBehavior.Strict);
+
+            mockEnvironment.Setup(env => env.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName))
+                .Returns("site1");
+
+            var workerValidationMiddleware = new WorkerValidationMiddleware(requestDelegate, mockEnvironment.Object,
+                _loggerFactory.CreateLogger<WorkerValidationMiddleware>());
+
+            var httpContext = CreateHttpContext();
+            var requestFeature = httpContext.Request.HttpContext.Features.Get<IHttpRequestFeature>();
+            requestFeature.Headers.Add(ScriptConstants.AntaresSiteDeploymentId, "site2");
+
+            await workerValidationMiddleware.Invoke(httpContext);
+
+            var allLogMessages = _loggerProvider.GetAllLogMessages();
+            Assert.Equal(2, allLogMessages.Count);
+        }
+
+        [Fact]
+        public async Task Allows_Valid_Requests()
+        {
+            RequestDelegate requestDelegate = async (HttpContext context) =>
+            {
+                await Task.Delay(0);
+            };
+
+            var mockEnvironment = new Mock<IEnvironment>(MockBehavior.Strict);
+
+            mockEnvironment.Setup(env => env.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName))
+                .Returns("site1");
+
+            var workerValidationMiddleware = new WorkerValidationMiddleware(requestDelegate, mockEnvironment.Object,
+                _loggerFactory.CreateLogger<WorkerValidationMiddleware>());
+
+            var httpContext = CreateHttpContext();
+            var requestFeature = httpContext.Request.HttpContext.Features.Get<IHttpRequestFeature>();
+            requestFeature.Headers.Add(ScriptConstants.AntaresSiteDeploymentId, "site1");
+
+            await workerValidationMiddleware.Invoke(httpContext);
+
+            var allLogMessages = _loggerProvider.GetAllLogMessages();
+            Assert.Equal(1, allLogMessages.Count);
+        }
+
+        private HttpContext CreateHttpContext()
+        {
+            string requestId = Guid.NewGuid().ToString();
+            var context = new DefaultHttpContext();
+            Uri uri = new Uri("http://functions.com");
+            var requestFeature = context.Request.HttpContext.Features.Get<IHttpRequestFeature>();
+            requestFeature.Method = "GET";
+            requestFeature.Scheme = uri.Scheme;
+            requestFeature.Path = uri.GetComponents(UriComponents.KeepDelimiter | UriComponents.Path, UriFormat.Unescaped);
+            requestFeature.PathBase = string.Empty;
+            requestFeature.QueryString = uri.GetComponents(UriComponents.KeepDelimiter | UriComponents.Query, UriFormat.Unescaped);
+
+            var headers = new HeaderDictionary();
+            headers.Add(ScriptConstants.AntaresLogIdHeaderName, new StringValues(requestId));
+            headers.Add("Host", new StringValues("hostheader"));
+            requestFeature.Headers = headers;
+
+            return context;
+        }
+    }
+}
